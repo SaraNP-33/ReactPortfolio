34b82997b315e7ac615abd481e4199d9df2cf7d0
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2020-07-04 12:55:25 -0700

    got masthead to render in react app

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
+import "./style.css";
+
+function Masthead(){
+    return(
+        <>
+<Jumbotron fluid className="masthead">
+ 
+  <Container className="TitleContainer">
+    <h1 className="name">Sara Neves Pereira</h1>
+    <h2 className="title">
+      Full Stack Web Developer 
+    </h2>
+  </Container>
+</Jumbotron>
+</>
+    )
+}
+
+export default Masthead;
---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Masthead from "../components/Jumbotron"
+
+function HomePage(){
+    return(
+        <Masthead/>
+    )
+}
+
+
+export default HomePage;
